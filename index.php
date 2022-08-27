1ab7c1d89256dfb47848451a9d9cb32788b690e9
DekuDeshayan <buraimoRalph129@gmail.com>
2022-08-27 15:24:57 +0200

    htaccess setup

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ session_start();
 require 'config.php';
 
 //define("BASE_URL", "http://localhost/SIGEFAv1.0");
-define("BASE_URL", "https://sigefa-ralph.herokuapp.com/");
+define("BASE_URL", "https://sigefa-ralph.herokuapp.com");
 
 spl_autoload_register(function($class){
 
