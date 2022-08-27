3851c884d27489835d6a83d694931410a3181808
DekuDeshayan <buraimoRalph129@gmail.com>
2022-08-27 14:53:44 +0200

    production env setup

--- config.php
+++ config.php
@@ -3,6 +3,12 @@ require 'environment.php';
 
 global $config;
 $config = array();
+
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$active_group = 'default';
+$query_builder = TRUE;
+
 //Se o ambiente for desenvolvimento os dados serao do servidor local
 if(ENVIRONMENT == "development"){
     $config['dbname']='SIGEFA';
@@ -10,14 +16,12 @@ if(ENVIRONMENT == "development"){
     $config['dbuser']='root';
     $config['dbpass']='';
 //Se o ambiente for producao os dados serao do servidor externo
-}else{
-    $config['dbname']='SIGEFA';
-    $config['host']='localhost';
-    $config['dbuser']='root';
-    $config['dbpass']='';
+}elseif(ENVIRONMENT == "production"){
+    $config['dbname']= substr($cleardb_url["path"],1);
+    $config['host']= $cleardb_url["host"];
+    $config['dbuser']= $cleardb_url["user"];
+    $config['dbpass']= $cleardb_url["pass"];
 }
-
-//NB:OS DADOS DO LOCALHOST E DO EXTERN HOST SAO DIFERENTES
 
 ?>
 
